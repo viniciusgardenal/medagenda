4bb57c9f21934acfaa8a86ae45baa74ef7648a76
Vinícius Gardenal <vinicius_sgardenal@hotmail.com>
2025-06-08 22:04:08 -0300

    update icons registroAtendimento

--- frontend/src/components/realizarAtendimentos/registroAtendimento.jsx
+++ frontend/src/components/realizarAtendimentos/registroAtendimento.jsx
@@ -121,21 +121,66 @@ const TableRow = ({ item, onRegister, onView, onEdit, onDelete }) => {
               className="text-blue-600 hover:text-blue-700 transition-colors"
               title="Visualizar Atendimento"
             >
-              <FaEye className="h-5 w-5" />
+              <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    />
+                  </svg>
             </button>
             <button
               onClick={() => onEdit(item)}
-              className="text-blue-600 hover:text-blue-700 transition-colors"
+              className="text-yellow-500 hover:text-yellow-700 transition-colors"
               title="Editar Atendimento"
             >
-              <FaEdit className="h-5 w-5" />
+              <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                    />
+                  </svg>
             </button>
             <button
               onClick={() => onDelete(item)}
-              className="text-red-600 hover:text-red-700 transition-colors"
+              className="text-red-500 hover:text-red-700 transition-colors"
               title="Excluir Atendimento"
             >
-              <FaTrash className="h-5 w-5" />
+              <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5-4h4m-4 4v12m4-12v12"
+                    />
+                  </svg>
             </button>
           </>
         )}
